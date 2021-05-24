701c983af4bf3c6947ed49a5a222be44d4c98434
realrobertdouglas <robert.douglas@hotmail.com>
2021-05-23 22:31:17 -0400

    added photos

---
+++ js/slr.0b647e.js
@@ -0,0 +1,14 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+var wl;
+
+!function(){if("Promise"in window){var e,t,r=document,n=function(){return r.createElement("link")},o=new Set,a=n(),i=a.relList&&a.relList.supports&&a.relList.supports("prefetch"),s=location.href.replace(/#[^#]+$/,"");o.add(s);var c=function(e){var t=location,r="http:",n="https:";if(e&&e.href&&e.origin==t.origin&&[r,n].includes(e.protocol)&&(e.protocol!=r||t.protocol!=n)){var o=e.pathname;if(!(e.hash&&o+e.search==t.pathname+t.search||".html"!=o.substr(-5)&&".html"!=o.substr(-5)&&"/"!=o.substr(-1)))return!0}},u=function(e){var t=e.replace(/#[^#]+$/,"");if(!o.has(t)){if(i){var a=n();a.rel="prefetch",a.href=t,r.head.appendChild(a)}else{var s=new XMLHttpRequest;s.open("GET",t,s.withCredentials=!0),s.send()}o.add(t)}},f=function(e){return e.target.closest("a")},p=function(t){var r=t.relatedTarget;r&&f(t)==r.closest("a")||e&&(clearTimeout(e),e=void 0)},l={capture:!0,passive:!0};r.addEventListener("touchstart",function(e){t=performance.now();var r=f(e);c(r)&&u(r.href)},l),r.addEventListener("mouseover",function(r){if(!(performance.now()-t<1200)){var n=f(r);c(n)&&(n.addEventListener("mouseout",p,{passive:!0}),e=setTimeout(function(){u(n.href),e=void 0},80))}},l)}}();
+
+$(function(){
+r=function(){dpi=window.devicePixelRatio;var e=document.querySelector('.js221');e.setAttribute('src',(dpi>1)?((dpi>2)?'images/20210226_250-1440.jpg':'images/20210226_250-960.jpg'):'images/20210226_250-480.jpg');
+var e=document.querySelector('.js222');e.setAttribute('src',(dpi>1)?((dpi>2)?'images/20210226_244-1440.jpg':'images/20210226_244-960.jpg'):'images/20210226_244-480.jpg');
+var e=document.querySelector('.js223');e.setAttribute('src',(dpi>1)?((dpi>2)?'images/20210226_254-1440.jpg':'images/20210226_254-960.jpg'):'images/20210226_254-480.jpg');
+var e=document.querySelector('.js224');e.setAttribute('src',(dpi>1)?((dpi>2)?'images/20210226_252-1440.jpg':'images/20210226_252-960.jpg'):'images/20210226_252-480.jpg');
+var e=document.querySelector('.js225');e.setAttribute('src',(dpi>1)?((dpi>2)?'images/20210226_238-2877.jpg':'images/20210226_238-1918.jpg'):'images/20210226_238-959.jpg');};
+if(!window.HTMLPictureElement){r();}
+
+});
